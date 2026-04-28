38d61bcbb242161c22d7590eca8d9016fddce6d3
Saifali Suleman <59619042+saifalisuleman@users.noreply.github.com>
2026-04-28 19:52:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import TubesCursor from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.19/build/cursors/tubes1.min.js";
+import TubesCursor from "tubes.min.js";
 
 const BRAND_COLORS = ["#378ADD", "#1D9E75", "#85B7EB"];
 const BRAND_LIGHTS = ["#0C447C", "#1D9E75", "#378ADD", "#E6F1FB"];
