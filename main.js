bd9159e0839dedb9843e155488b3e1ec4e0c70f7
Saifali Suleman <59619042+saifalisuleman@users.noreply.github.com>
2026-05-01 19:42:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import TubesCursor from "/tubes.min.js";
 
-const BRAND_COLORS = ["#2dd47a", "#1a8a4e", "#97C459"];
-const BRAND_LIGHTS = ["#0d3d24", "#1a8a4e", "#2dd47a", "#e8efe9"];
+const BRAND_COLORS = ["#378ADD", "#85B7EB", "#0C447C"];
+const BRAND_LIGHTS = ["#042C53", "#0C447C", "#378ADD", "#E6F1FB"];
 
 const canvas = document.getElementById('canvas');
 
